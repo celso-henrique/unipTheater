c95d600b73530297e10a95070da2c3107ae653a2
Celso Henrique <celso.henrique@hotmail.com.br>
2018-11-19 15:25:03 -0200

    Finished mvp

--- teatroUnip.c
+++ teatroUnip.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// Limpa a tela
+// Definição das funções para referência nas funções
+void mainMenu();
+void sellTicketMenu();
+
+// Definição das sessões
+struct TheaterSession  {
+  char title[50];
+  int soldPlaces;
+  int soldValues[30];
+};
+
+struct TheaterSession session1 = {.title = "O Fantasma da Ópera", .soldPlaces = 0};
+struct TheaterSession session2 = {.title = "Romeu e Julieta", .soldPlaces = 0};
+
+// Função para limpar a tela
 void clearScreen() {
   system("@cls||clear");
 }
 
+// Função que exibe "pressione qualquer tecla para continuar..."
+void pressAnyKey () {
+  printf("Pressione qualquer tecla para continuar...");
+  while (getchar() != '\n');
+  getchar();
+}
+
 // Renderiza o cabeçalho
-static void renderHeader() {
+void renderHeader() {
+  clearScreen();
   printf("\n\n  _______         _               _    _       _       \n");
   printf(" |__   __|       | |             | |  | |     (_)      \n");
   printf("    | | ___  __ _| |_ _ __ ___   | |  | |_ __  _ _ __  \n");
@@ -20,7 +43,7 @@ static void renderHeader() {
 };
 
 // Renderiza menus e retorna a opção escolhida
-static int renderMenu(char* title, char* items[], int size) {
+int renderMenu(char* title, char* items[], int size) {
   int i;
   int option;
   
@@ -34,7 +57,6 @@ static int renderMenu(char* title, char* items[], int size) {
   scanf("%d",&option); 
 
   if (option > size || option < 1) {
-    clearScreen();
     renderHeader();
     return renderMenu(title, items, size);
   }
@@ -42,27 +64,74 @@ static int renderMenu(char* title, char* items[], int size) {
   return option;
 }
 
-// Venda de ingressos
-static void sellTicket() {
+// Seleção de tipo de ingresso
+void finishTicket(struct TheaterSession *session) {
   int selectedOption;
-  char* menuOptions[] = {"O Fantasma da Ópera", "Romeu e Julieta", "Voltar ao menu principal"};
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  char* menuOptions[] = {"Inteira (R$30,00)", "Voltar para seleção da peça"};
 
-  clearScreen();
   renderHeader();
 
+  if (session->soldPlaces < 30) {
+    selectedOption = renderMenu(
+      "Selecione o tipo de ingresso:",
+      menuOptions,
+      sizeof(menuOptions) / sizeof(char*)
+    );
+
+    switch(selectedOption) {
+    case 1:
+      session->soldPlaces = session->soldPlaces + 1;
+      session->soldValues[session->soldPlaces - 1] = 30;
+      break;
+    }
+
+    renderHeader();
+    printf("Venda efetuada com sucesso, lugares disponíveis: %d\n\n", 30 - session->soldPlaces);
+    printf("Ticket: \n\n");
+    printf("Peça: %s\n", session->title);
+    printf("Assento: %d\n", session->soldPlaces);
+    printf("Data: %d/%d/%d - 21:00\n\n", tm.tm_mday, tm.tm_mon,tm.tm_year+1900);
+    pressAnyKey();
+    mainMenu();
+  } else {
+    printf("Sessão esgotada, escolha outra sessão. \n\n");
+    pressAnyKey();
+    sellTicketMenu();
+  }
+}
+
+// Menu de venda de ingressos
+void sellTicketMenu() {
+  int selectedOption;
+  char* menuOptions[] = {session1.title, session2.title, "Voltar ao menu principal"};
+
+  renderHeader();
   selectedOption = renderMenu(
-    "Selecione a peça que deseja vender ingressos:",
+    "Sessões para hoje às 21:00,\nselecione a peça que deseja vender ingressos:",
     menuOptions,
     sizeof(menuOptions) / sizeof(char*)
   );
+
+  switch(selectedOption) {
+  case 1:
+    finishTicket(&session1);
+    break;
+  case 2:
+    finishTicket(&session2);
+    break;
+  case 3:
+    mainMenu();
+    break;
+  }
 }
 
 // Menu principal
-static void mainMenu() {
+void mainMenu() {
   int selectedOption;
-  char* menuOptions[] = {"Vender ingressos"};
+  char* menuOptions[] = {"Vender ingressos", "Sair"};
 
-  clearScreen();
   renderHeader();
   selectedOption = renderMenu(
     "Seja bem vindo ao sistema de vendas de ingressos!",
@@ -70,11 +139,17 @@ static void mainMenu() {
     sizeof(menuOptions) / sizeof(char*)
   );
 
-  if (selectedOption == 1) {
-    sellTicket();
+  switch(selectedOption) {
+  case 1:
+    sellTicketMenu();
+    break;
+  case 2:
+    exit(0);
+    break;
   }
 }
 
+// Função principal
 int main(){
   mainMenu();
   return 0;
